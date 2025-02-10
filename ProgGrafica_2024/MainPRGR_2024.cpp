e060e59449f990d03587c9604f114a6f7e4aaf89
Marta21GH <martaprietorodriguezdevera@gmail.com>
2025-02-10 17:59:07 +0100

    main

--- ProgGrafica_2024/MainPRGR_2024.cpp
+++ ProgGrafica_2024/MainPRGR_2024.cpp
@@ -5,6 +5,7 @@
 #include "libprgr/InputManager.h"
 #include "libprgr/Render.h"
 #include "libprgr/Object3D.h"
+
 using namespace libPRGR;
 
 int main(int argc, char** argv)
@@ -15,13 +16,18 @@ int main(int argc, char** argv)
 
 	//Crear una variable "triangulo" de tipo Object3D, y llamar a su método “createTriangle” para que 
 	//	almacene los vértices e identificadores de vértices para un triángulo
+	Object3D* triangulo = new Object3D();
+	triangulo->createTriangle();
 	
 	//Dibujarlo en el buffer(putObject), y mostrar el buffer por ventana(mainLoop)
+	rend->putObject(triangulo);
 	
 	//Cuando se pulsen las teclas “A” o “D” el triángulo girará acorde con lo indicado en su método
 	//	“update”.
-
 	rend->mainLoop();
+
+	//finalizar GLFW y liberar recursos
+	glfwTerminate();
 
 	return 0;
 }
